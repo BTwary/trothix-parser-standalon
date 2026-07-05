eb255c74f415186d7e5ce2d6e411218cfb48fa2b
DesignLab <bhaskartiwary44@gmail.com>
2026-07-05 11:11:31 +0530

    Delete Trothix-Parser-Engine directory

--- Trothix-Parser-Engine/add_scripts.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('index.html', 'utf8');
-const replacement = `<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js"></script>
-<script src="https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js"></script>
-<script src="./assets/js/pdfProcessor.js"></script>
-</body>`;
-c = c.replace('</body>', replacement);
-fs.writeFileSync('index.html', c);
